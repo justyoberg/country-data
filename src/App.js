dfa9db78876c083cbd14d108133b4abe54bff140
Justy Oberg <justyoberg1@gmail.com>
2023-04-02 20:29:20 -0700

    Add show button

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import axios from "axios";
 function App() {
   const [countries, setCountries] = useState(null);
   const [newSearch, setNewSearch] = useState("");
+  const [shownCountry, setShownCountry] = useState(null);
 
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
       .then((response) => setCountries(response.data));
   }, []);
+
+  const resetShown = () => {
+    setShownCountry(null);
+  }
+
+  useEffect(() => {
+    resetShown()
+  }, [newSearch])
 
   if (!countries) return null;
 
@@ -22,11 +31,19 @@ function App() {
     setNewSearch(event.target.value);
   };
 
+  const showCountry = (country) => {
+    setShownCountry(country);
+  }
+
   return (
     <div>
       <h1>Find countries</h1>
       <input value={newSearch} onChange={handleChange} />
-      <Countries countries={countriesToShow} />
+      <Countries countries={countriesToShow} 
+                  shown={shownCountry} 
+                  setShown={showCountry}
+                  resetShown={resetShown}
+      />
     </div>
   );
 }
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,30 +1,44 @@
-const Countries = ({ countries }) => {
-  console.log(countries)
-  if (countries.length <= 0)
-    return <p>No matches found</p>
-  else if (countries.length > 10)
-    return <p>Too many matches, specify another filter</p>;
-  else if (countries.length === 1) {
+const Countries = ({ countries, shown, setShown }) => {
+  const showCountry = (country) => {
     return (
       <div>
-        <h1>{countries[0].name.common}</h1>
-        <p>Capital: {countries[0].capital[0]}</p>
-        <p>Area: {countries[0].area}</p>
+        <h1>{country.name.common}</h1>
+        <p>Capital: {country.capital[0]}</p>
+        <p>Area: {country.area}</p>
         <h3>Languages:</h3>
         <ul>
-          {Object.values(countries[0].languages).map(item => {
-            return <li>{item}</li>
+          {Object.values(country.languages).map((item) => {
+            return <li key={item}>{item}</li>;
           })}
         </ul>
-        <img src={countries[0].flags.png} alt="flag"/>
+        <img src={country.flags.png} alt="flag" />
       </div>
     );
+  };
+
+  if (shown) {
+    return showCountry(shown);
+  }
+
+  if (countries.length === 250) {
+    return <p>Enter a country</p>
+  } else if (countries.length <= 0) {
+    return <p>No matches found</p>;
+  } else if (countries.length > 10) {
+    return <p>Too many matches, specify another filter</p>;
+  } else if (countries.length === 1) {
+    return showCountry(countries[0]);
   }
 
   return (
     <ul>
-      {countries.map((item) => {
-        return <li key={item.name.common}>{item.name.common}</li>
+      {countries.map((country) => {
+        return (
+          <li key={country.name.common}>
+            {country.name.common}
+            <button onClick={() => setShown(country)}>show</button>
+          </li>
+        );
       })}
     </ul>
   );
